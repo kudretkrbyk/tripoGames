64e625b5aed2d63f681b5dfc33c4a9e949ff0c90
kudretkrbyk <kudretkrbyk@gmail.com>
2024-06-15 08:30:49 +0300

    navbar menu

--- tripoGames/src/components/Navbar.jsx
+++ tripoGames/src/components/Navbar.jsx
@@ -6,7 +6,15 @@ export default function Navbar() {
       </div>
       <div className="flex  items-center justify-center  bg-blue-400 shadow-xl rounded-b z-30">
         <div className="bg-[#da392b] p-5 rounded-bl">Anasayfa </div>
-        <div className=" hover:bg-[#da392b] p-5">Oyunlar</div>
+        <div className=" hover:bg-[#da392b] p-5 relative group">
+          Oyunlar
+          <div className="flex flex-col absolute bg-white p-3 rounded-xl shadow-xl top-16 left-0 text-nowrap opacity-0 group-hover:opacity-100 duration-700">
+            {" "}
+            <div>Kipon</div>
+            <div>Robotrix</div>
+            <div>Treasure Box</div>
+          </div>
+        </div>
         <div className=" hover:bg-[#da392b] p-5">Kariyer</div>
         <div className=" hover:bg-[#da392b] p-5">Hakkında</div>
         <div className=" hover:bg-[#da392b] p-5">İletişim</div>
