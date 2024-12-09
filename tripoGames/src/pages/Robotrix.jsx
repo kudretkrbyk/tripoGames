cb641e963bb2b9543285f6d8cf68790ea6b2e1b7
kudretkrbyk <kudretkrbyk@gmail.com>
2024-12-09 13:01:01 +0300

    added responsive

--- tripoGames/src/pages/Robotrix.jsx
+++ tripoGames/src/pages/Robotrix.jsx
@@ -19,32 +19,32 @@ export default function Robotrix() {
 
         <div
           ref={divRef1}
-          className={`absolute top-0 right-1/2 transition-transform duration-700 ${
+          className={`absolute top-5 left-5  transition-transform duration-700 ${
             hasIntersected1 ? "translate-x-0" : "-translate-x-full"
-          } w-1/3 bg-white shadow-xl flex flex-col gap-10 rounded-xl p-10`}
+          } w-1/2 md:w-2/5  xl:w-1/3 bg-white shadow-xl flex flex-col gap-10 rounded-xl p-10`}
         >
           <div className="flex items-center justify-start gap-2">
             {" "}
             <div className="bg-cover bg-center w-20 h-20 rounded-xl bg-[url(https://static.wixstatic.com/media/c837a6_1aa2b71b79dd4d99aac8c7a98d07c586~mv2.jpg/v1/crop/x_1579,y_198,w_1081,h_1081/fill/w_98,h_98,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/shutterstock_1711821976.jpg)]  "></div>{" "}
-            <div>
+            <div className="w-full">
               {" "}
               <div>Kipon</div>
               <div>Gündelik-Ücretsiz</div>
             </div>
           </div>
-          <div className="text-5xl font-bold">
+          <div className="md:text-3xl xl:text-5xl font-bold w-full">
             Sınırsız Heyecanı Parmak Uçlarınıza Getiren Benzersiz Bir Oyun.
           </div>
-          <div>
+          <div className="w-full">
             Bu, bir paragraf. Kendi metninizi eklemek için tıklayın. Burası, bir
             hikâye anlatmak ve kullanıcılarınıza kendinizi tanıtmak için harika
             bir yer.
           </div>
         </div>
       </div>
-      <div className="flex flex-col w-full p-10 gap-5">
+      <div className="flex flex-col w-full p-5 md:p-10 gap-5 ">
         <div className="font-bold">Ekran Görüntüleri</div>
-        <div className="flex items-center justify-center gap-10 ">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-10 ">
           <div className="w-full  group overflow-hidden rounded-3xl ">
             <div className="w-full h-96 rounded-3xl group-hover:scale-110 duration-1000 bg-center  bg-cover bg-[url(https://static.wixstatic.com/media/c837a6_95068cdc344b47c4a49a628164390317~mv2.jpg/v1/fill/w_1919,h_1080,q_90/c837a6_95068cdc344b47c4a49a628164390317~mv2.jpg)]   "></div>
           </div>
@@ -53,27 +53,27 @@ export default function Robotrix() {
           </div>
         </div>
       </div>
-      <div className="flex flex-col w-full p-10">
-        <div className="flex items-center justify-start gap-5 p-5">
-          <div className="w-60 h-48 bg-white rounded-xl shadow-xl flex items-center justify-center">
+      <div className="flex flex-col w-full p-5 md:p-10">
+        <div className="flex items-center md:justify-start justify-center gap-5 p-5">
+          <div className="w-60 h-20 md:h-48 bg-white rounded-xl shadow-xl flex items-center justify-center ">
             <div className="bg-red-500 w-48 h-10 rounded-full flex items-center justify-between p-1">
               <img className="w-8 h-8 p-2" src={StarIcon} alt="Icon" />
-              <div className=" w-36 h-8 rounded-full bg-white flex items-center justify-center">
+              <div className=" w-36 h-8 rounded-full bg-white flex items-center justify-center ">
                 <div>4.1 Puan</div>
               </div>
             </div>
           </div>
           <div>Yorumlar</div>
         </div>
-        <div className="flex border border-black w-full h-96">
-          <div className="w-1/3 border-r border-black flex flex-col items-start justify-center gap-5 p-10">
+        <div className="flex border border-black w-full h-[500px] md:h-96">
+          <div className="w-full h-full border-r border-black flex flex-col items-start justify-between gap-5 p-5 md:p-10">
             <div>
               “Bu, bir müşteri görüşü. Siz ve hizmetleriniz hakkında olumlu
               şeyler anlatan bir metin ekleyin.”
             </div>
             <div className="font-bold text-xl">Cenk Rua</div>
           </div>
-          <div className="w-1/3 border-r border-black flex flex-col items-start justify-center gap-5 p-10">
+          <div className="w-full h-full border-r border-black flex flex-col items-start justify-between gap-5 p-5 md:p-10">
             <div>
               “Bu, bir müşteri görüşü. Siz ve hizmetleriniz hakkında olumlu
               şeyler anlatan bir metin eklemek için tıklayın. Müşterileriniz
@@ -81,7 +81,7 @@ export default function Robotrix() {
             </div>
             <div className="font-bold text-xl">Emir Akkan</div>
           </div>
-          <div className="w-1/3 flex flex-col items-start justify-center gap-5 p-10">
+          <div className="w-full h-full flex flex-col items-start justify-between gap-5 p-5 md:p-10">
             <div>
               “Bu, bir müşteri görüşü. Siz ve hizmetleriniz hakkında olumlu
               şeyler anlatan bir metin ekleyin.”
@@ -106,8 +106,8 @@ export default function Robotrix() {
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center gap-10 p-5    w-full">
-        <div className="bg-white w-1/3 flex flex-col gap-10 items-center justify-center shadow-2xl p-10">
+      <div className="flex flex-col items-center justify-center gap-10 p-5    w-full border border-black">
+        <div className="bg-white w-full md:w-2/3 flex flex-col gap-10 items-center justify-center shadow-2xl p-10">
           <div className="text-5xl font-bold">Hemen Oynayın</div>
           <div className="flex w-full items-center justify-center gap-5">
             {" "}
